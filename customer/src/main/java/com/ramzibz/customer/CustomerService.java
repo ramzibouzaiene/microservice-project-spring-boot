12203d841fd37049236a518b70cbd9331935b51c
Ramzi BOUZAIENE <ramzi.bouzaiene@proxym-it.com>
2024-01-21 14:03:54 +0100

    communication between services

--- customer/src/main/java/com/ramzibz/customer/CustomerService.java
+++ customer/src/main/java/com/ramzibz/customer/CustomerService.java
@@ -2,18 +2,28 @@ package com.ramzibz.customer;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
                 .lastName(request.lastName())
                 .email(request.email())
                 .build();
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http:localhost:8081/api/v1/fraud-check/{sutomerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+        if(fraudCheckResponse.isFraudster()){
+            throw new IllegalStateException("fraudster");
+        }
     }
 }
