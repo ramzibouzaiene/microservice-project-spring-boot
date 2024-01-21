aaa5127e7f71e19c8d4493eed2ce4f39c4a587e4
Ramzi BOUZAIENE <ramzi.bouzaiene@proxym-it.com>
2024-01-21 12:46:50 +0100

    congifure the data base for customer service

--- customer/src/main/java/com/ramzibz/customer/CustomerController.java
+++ customer/src/main/java/com/ramzibz/customer/CustomerController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public record CustomerController(CustomerService customerService) {
 
     @PostMapping
-    public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest){
+    public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest) {
         log.info("new customer registration {]", customerRegistrationRequest);
         customerService.registerCustomer(customerRegistrationRequest);
     }
